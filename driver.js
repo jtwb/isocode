8fa07cb6058af87c0c0b8641fe8a5bf2cacd9139
Jason Benterou <jason.benterou@gmail.com>
2013-11-30 15:56:24 -0800

    Render URL to HTML string

---
+++ driver.js
@@ -0,0 +1,51 @@
+var page = require('webpage').create();
+var system = require('system');
+
+//
+// Usage and argparse
+//
+var usage = 'Usage: phantomjs driver.js [ URL ]';
+var args = Array.prototype.slice.call(system.args, 0);
+if (args.length > 1) {
+  args.url = args[1];
+} else {
+  console.log(usage);
+  phantom.exit();
+}
+
+
+// 
+// Utilities
+// 
+var isocodePhantomPrefixPresent = function(msg) {
+  return /^phantom|/.test(msg)
+};
+
+var isocodePhantomPrefixStrip = function(msg) {
+  var prefix = 'phantom|'.length;
+  return msg.substr(prefix);
+};
+
+
+
+//
+// Emit string
+//
+page.onConsoleMessage = function(msg) {
+  if (isocodePhantomPrefixPresent(msg)) {
+    console.log(isocodePhantomPrefixStrip(msg));
+  }
+};
+
+
+
+//
+// Drive PhantomJS
+// 
+console.log(args.url);
+page.open(args.url, function() {
+  page.evaluate(function() {
+    console.log($('html').html());
+  });
+  phantom.exit();
+});
