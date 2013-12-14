33fdc8c2902d353576fae0eb545a7a6add5c4051
Jason Benterou <jason.benterou@gmail.com>
2013-12-13 20:31:09 -0800

    Support reading from stdin and improve executables

--- drivers/phantomjs/driver.js
+++ drivers/phantomjs/driver.js
@@ -1,18 +1,79 @@
 var page = require('webpage').create();
 var system = require('system');
 
+// plain driver reads the page from stdin using this strategy:
+// 1 Spawn a webserver on some unix socket (or local port)
+// 2 Tell phantom to fetch page from said unix socket (or local port)
+// 3 On data recieved from isocode, xmit data to phantom
+// 4 On complete, phantom should send EOF through the pipe
+//
+// Isocode child stdin -> Webserver IN -> Webserver RESPONSE
+// -> Phantom PAGE IN ... Phantom result -> Phantom STDOUT -> Isocode child stdout
+//
+// Plan B
+//
+// Open file:///dev/stdin
+// - note, phantom may not be waiting for EOF on stdin
+//
+// Plan C
+//
+// Pipe /dev/stdin to a /tmp file /tmp/:hash
+// Open file:///tmp/:hash
+// 
+
 //
 // Usage and argparse
 //
-var usage = 'Usage: phantomjs driver.js [ URL ]';
+var usage = 'Usage: phantomjs driver.js [ URL | "-" ]';
 var args = Array.prototype.slice.call(system.args, 0);
-if (args.length > 1) {
-  args.url = args[1];
-} else {
+
+console.log(args);
+if (args.length < 2 || args[1] === '-') {
+  
+  // no arguments or first argument '-' implies read stdin
+  args.stdin = true;
+} else if (/--help|-h|--version|-v/.test(args[1])) {
   console.log(usage);
   phantom.exit();
+} else {
+  args.url = args[1];
 }
 
+
+// NOTES on working with PhantomJS Streams (e.g. STDIN)
+// ... as of 1.9.2 ...
+// * PhantomJS File, Streams are based on QT QFile http://qt-project.org/doc/qt-4.8/qiodevice.html#readAll
+// * CPP Source:
+//   https://github.com/ariya/phantomjs/blob/4989445e714bb97fe49ef8c00ccbce8b6cfbfbb0/src/filesystem.cpp
+//   https://github.com/ariya/phantomjs/blob/4989445e714bb97fe49ef8c00ccbce8b6cfbfbb0/src/filesystem.h
+//
+// Methods available
+// open
+// read (bytes)
+// read (-1 -> readall)
+// atEnd() -> bool
+// close
+//
+// while !atEnd
+//  read (bufsize)
+// 
+// TODO explore streaming HTML bytes to phantom in 0x4000b blocks
+
+
+//
+// Start webserver
+// iff reading from stdin
+//
+if (args.stdin) {
+  var webserver = require('webserver');
+  var server = webserver.create();
+  var service = server.listen(9080, function(request, response) {
+    var body = system.stdin.read(-1);
+    response.statusCode = 200;
+    response.write(body);
+    response.close();
+  });
+}
 
 // 
 // Utilities
@@ -39,6 +100,9 @@ page.onConsoleMessage = function(msg) {
 };
 
 
+//
+// PhantomJS in-page processing
+//
 var processPage = function() {
 
   var getXHRLogConfig = function() {
@@ -63,7 +127,6 @@ var processPage = function() {
   };
 
   var getPageHTML = function() {
-    // $('html').html();
     return (document.doctype ? document.doctype + '\n' : '') +
       document.documentElement.outerHTML;
   };
@@ -76,7 +139,7 @@ var processPage = function() {
 //
 // Drive PhantomJS
 // 
-page.open(args.url, function() {
+page.open(args.stdin ? 'http://localhost:9080/' : args.url, function() {
   page.evaluate(processPage);
   phantom.exit();
 });
